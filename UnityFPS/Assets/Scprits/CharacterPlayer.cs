20ad0a80eeeea35a836f51141d6f89339d20a248
yuhongbo168 <yhb776@163.com>
2022-02-22 17:51:46 +0800

    modifi scene

---
+++ UnityFPS/Assets/Scprits/CharacterPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CharacterPlayer : MonoBehaviour
+{
+
+    public PlayerController PC;
+
+    
+
+    private void Awake()
+    {
+        PC = GetComponent<PlayerController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityFPS/Assets/Scprits/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rd;
+
+    private PlayerController m_pc;
+    public PlayerController Pc
+    {
+        get
+        {
+            return m_pc;
+        }
+    }
+
+   
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        m_pc = GetComponent<PlayerController>();
+        rd = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
